dc1ff5022d9b1dfd3921ecad715548585e4d1eb1
mikuriya <mickly9@hotmail.com>
2019-07-24 15:29:26 +0900

    add zmq_sender

--- contents_sdk/sudare.cpp
+++ contents_sdk/sudare.cpp
@@ -22,24 +22,23 @@ class TimeMeter {
     std::cout << m_symbol << " : " << diff << "ms" << std::endl;
   }
 };
+template <typename Publisher>
 class Sudare {
   std::chrono::time_point<std::chrono::system_clock> m_last_sleep_time;
   enum mode { rectangular_mode = 0, polar_mode = 1 } m_mode;
   sudare::rectangular m_rect;
   sudare::polar m_polar;
   sudare::converter m_conv;
-  sudare::zmq_initializer m_zmq_init;
-  sudare::zmq_client m_zmq_client;
+  Publisher const& m_pub;
 
  public:
-  explicit Sudare(const char* dst)
+  explicit Sudare(Publisher const& pub)
       : m_last_sleep_time(std::chrono::system_clock::now()),
         m_mode(rectangular_mode),
         m_rect(RECTANGULAR_WIDTH, RECTANGULAR_HEIGHT, RECTANGULAR_DEPTH),
         m_polar(POLAR_ANGLES, POLAR_RADIUS, POLAR_HEIGHT),
         m_conv(m_rect, m_polar),
-        m_zmq_init(),
-        m_zmq_client(m_zmq_init.context(), dst) {}
+        m_pub(pub) {}
   void set_led_rect(int x, int y, int z, int rgb) {
     m_rect.set(x, y, z, rgb);
     m_mode = rectangular_mode;
@@ -57,9 +56,9 @@ class Sudare {
       TimeMeter t("convert");
       m_conv();
     }
-    TimeMeter t("zmq send");
-    m_zmq_client.send(reinterpret_cast<const char*>(m_polar.data()),
-                      m_polar.size());
+    TimeMeter t("publish");
+    m_pub.publish(reinterpret_cast<const char*>(m_polar.data()),
+                  m_polar.size());
   }
   void sleep(int ms) {
     std::chrono::time_point now = std::chrono::system_clock::now();
@@ -72,12 +71,24 @@ class Sudare {
     m_last_sleep_time = std::chrono::system_clock::now();
   }
 };
-std::shared_ptr<Sudare> s;
+class zmq_sender {
+  sudare::zmq_initializer m_init;
+  sudare::zmq_client m_client;
+
+ public:
+  zmq_sender(const char* dst) : m_init(), m_client(m_init.context(), dst) {}
+  void publish(const char* data, size_t size) const {
+    m_client.send(data, size);
+  }
+};
+std::shared_ptr<zmq_sender> s_sender;
+std::shared_ptr<Sudare<zmq_sender>> s;
 }  // namespace
 
 int sudare_init_sdk(const char* dst) {
   try {
-    s = std::make_shared<Sudare>(dst);
+    s_sender = std::make_shared<zmq_sender>(dst);
+    s = std::make_shared<Sudare<zmq_sender>>(*s_sender);
     return 0;
   } catch (std::exception const& e) {
     std::cerr << e.what() << std::endl;
