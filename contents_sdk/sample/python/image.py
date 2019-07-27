e4e6d1d20861c5de39095a2bd1f3b64a80ca956b
御厨 洋 <mickly9@MacBookAirHiroshi-2.local>
2019-07-27 19:47:54 +0900

    ドラえもんを角度関係なく平面に表示するようにした

--- contents_sdk/sample/python/image.py
+++ contents_sdk/sample/python/image.py
@@ -20,16 +20,20 @@ cv2.waitKey(1)
 img = cv2.flip(img, 0)
 img = cv2.flip(img, 1)
 
-a = 0
 while True:
   lib.sudare_clear()
-  for x in range(0, size[0]):
-    for y in range(0, size[1]):
-      c = img[y][x]
-      rgb = (c[2] << 16) | (c[1] << 8) | c[0]
-      for r in range(13, 15):
-        lib.sudare_set_led_polar(a + x, r, y, rgb)
+  for a in range(0, 30):
+    for r in range(0, 15):
+      x0 = 14 - r
+      x1 = 15 + r
+      for y in range(0, size[1]):
+        c = img[y][x0]
+        rgb = (c[2] << 16) | (c[1] << 8) | c[0]
+        lib.sudare_set_led_polar(a, r, y, rgb)
+        c = img[y][x1]
+        rgb = (c[2] << 16) | (c[1] << 8) | c[0]
+        lib.sudare_set_led_polar(a + 30, r, y, rgb)
   if lib.sudare_send() is not 0:
     sys.exit(1)
   lib.sudare_sleep(100)
-  a += 1
+  
