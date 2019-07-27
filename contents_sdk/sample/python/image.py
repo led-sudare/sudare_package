1b9c312ab4840da82084c81a8a2a05ee3301bec2
御厨 洋 <mickly9@MacBookAirHiroshi-2.local>
2019-07-27 10:20:59 +0900

    無限ループ

--- contents_sdk/sample/python/image.py
+++ contents_sdk/sample/python/image.py
@@ -9,7 +9,7 @@ if img is None:
 
 lib = ctypes.cdll.LoadLibrary('libsudare.so')
 
-if lib.sudare_init_sdk('*:5511') != 0:
+if lib.sudare_init_sdk('*:5511') is not 0:
   sys.exit(1)
 
 size = (30, 100)
@@ -20,7 +20,8 @@ cv2.waitKey(1)
 img = cv2.flip(img, 0)
 img = cv2.flip(img, 1)
 
-for a in range(0, 1000):
+a = 0
+while True:
   lib.sudare_clear()
   for x in range(0, size[0]):
     for y in range(0, size[1]):
@@ -28,6 +29,7 @@ for a in range(0, 1000):
       rgb = (c[2] << 16) | (c[1] << 8) | c[0]
       for r in range(13, 15):
         lib.sudare_set_led_polar(a + x, r, y, rgb)
-  if lib.sudare_send() != 0:
+  if lib.sudare_send() is not 0:
     sys.exit(1)
   lib.sudare_sleep(100)
+  a += 1
